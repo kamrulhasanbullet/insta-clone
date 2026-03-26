7dce314250abe855f8e4fab80e84e863514ada9c
Kamrul Islam <bullethasankamrul@gmail.com>
2026-03-26 13:27:14 +0600

    add sidebar and mobilenav

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import { Sidebar } from "@/components/layout/Sidebar";
+import { MobileNav } from "@/components/layout/MobileNav";
+
 export default function MainLayout({
   children,
 }: {
@@ -5,11 +8,13 @@ export default function MainLayout({
 }) {
   return (
     <div className="min-h-screen bg-gray-50">
+      <Sidebar />
       <main className="md:ml-64 xl:ml-72 min-h-screen">
         <div className="max-w-screen-sm mx-auto px-4 pb-20 md:pb-0">
           {children}
         </div>
       </main>
+      <MobileNav />
     </div>
   );
 }
